ddce969f7fcc5695cbfc654c1755a6c4fdcde160
yaizuuuu <p.i.o.y.m.a.a@gmail.com>
2019-09-14 15:39:17 +0900

    ビュー: カスタムテンプレートを使用してみる

---
+++ layouts/blog.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <div>これはblog.vueでlayoutでえす</div>
+    <nuxt/>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
---
+++ pages/post.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <div>これはpost.vueでpagesでえす</div>
+  </div>
+</template>
+<script>
+export default {
+  layout: 'blog'
+  // ページコンポーネントの定義
+}
+</script>
